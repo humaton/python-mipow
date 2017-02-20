dddd85aa477eda368936d6d95e67cd3d0831b08f
Andreas Hohler <east7074@gmail.com>
2017-02-21 00:09:54 +0100

    changed package name to right one

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ dynamic_requires = []
 version = 0.3
 
 setup(
-    name='mipow-comet',
+    name='mipow_comet',
     version=0.3,
     author='Andreas Hohler',
     author_email='east7074@gmail.com',
     url='https://github.com/papagei9/python-mipow',
     packages=find_packages(),
-    download_url='https://github.com/amahlaka/python-mipow/tarball/0.3',
+    download_url='https://github.com/papagei9/python-mipow/tarball/0.3',
     scripts=[],
     description='Python API for controlling Mipow COMET LED stripes',
     classifiers=[
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import mipow
+import mipow_comet
 
 bulb = mipow.mipow("70:44:4B:14:AC:E6")
 bulb.connect()
